82407de2658a9bcffbf53783603c2dbca138328c
edward <edward@pdx.edu>
2019-01-01 15:12:51 -0800

    Movement  -basic
    selected characters now move when the player right clicks

--- Assets/Scripts/CharacterDetails.cs
+++ Assets/Scripts/CharacterDetails.cs
@@ -8,6 +8,12 @@ public class CharacterDetails : MonoBehaviour {
     public int maxStam = 100;
     public int maxEnergy = 100;
 
+    bool hasTarget = false;
+    Rigidbody characterBody;
+    Vector3 movement;
+    RaycastHit target;
+    UnityEngine.AI.NavMeshAgent nav;
+
     Animator anim;
     int health, stam, energy;
 
@@ -16,8 +22,18 @@ public class CharacterDetails : MonoBehaviour {
         health = maxHealth;
         stam = maxStam;
         energy = maxEnergy;
-        Debug.Log("Energy = " + energy + ", MaxEnergy =" + maxEnergy);
+        //Debug.Log("Energy = " + energy + ", MaxEnergy =" + maxEnergy);
         anim = GetComponent<Animator>();
+        characterBody = GetComponent<Rigidbody>();
+        nav = GetComponent<UnityEngine.AI.NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        if(hasTarget && health > 0)
+        {
+            nav.SetDestination(target.point);
+        }
     }
 
     public void TakeDamage(int damage)
@@ -27,6 +43,17 @@ public class CharacterDetails : MonoBehaviour {
         {
             anim.SetTrigger("IsDead");
         }
+    }
+
+    public void Move(RaycastHit newTarget)
+    {
+        target.point = newTarget.point;
+        hasTarget = true;
+        /*
+        Debug.Log("Move to " + target.point.x + "," + target.point.y + "," + target.point.z);
+        movement.Set(target.point.x, target.point.y, target.point.z);
+        movement = movement.normalized * speed * Time.deltaTime;
+        characterBody.MovePosition(transform.position + movement);*/
     }
 
     public int Health()
--- Assets/Scripts/CharacterPanelControl.cs
+++ Assets/Scripts/CharacterPanelControl.cs
@@ -11,9 +11,11 @@ public class CharacterPanelControl : MonoBehaviour {
     GameObject statusPanel;
     Text health, stamina, energy;
     Slider healthSlider, stamSlider, energySlider;
+    int terrainMask;
 
     void Awake()
     {
+        terrainMask = LayerMask.GetMask("Terrain");
         statusPanel = GameObject.Find("StatusPanel");
         health = GameObject.FindGameObjectWithTag("Health").GetComponent<Text>();
         stamina = GameObject.FindGameObjectWithTag("Stamina").GetComponent<Text>();
@@ -40,24 +42,13 @@ public class CharacterPanelControl : MonoBehaviour {
 
     public void Update()
     {
-        Ray ray;
-        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
         if (Input.GetMouseButtonUp(0))
         {
-            if (Physics.Raycast(ray, out hit))
-            {
-                if (hit.transform.tag == "Character")
-                {
-                    character = GameObject.Find(hit.transform.name);
-                    Debug.Log("We hit " + hit.transform.name);
-                }
-                else
-                {
-                    character = null;
-                    Debug.Log("We hit nothing.");
-                }
-            }
+            LeftClick();
+        }
+        else if (Input.GetMouseButtonUp(1))
+        {
+            RightClick();
         }
 
         if (!character)
@@ -65,7 +56,14 @@ public class CharacterPanelControl : MonoBehaviour {
             HidePanel();
             return;
         }
+
         ShowPanel();
+        UpdatePanel();
+        
+    }
+
+    void UpdatePanel()
+    {
         CharacterDetails detail = character.GetComponent<CharacterDetails>();
         health.text = detail.Health() + "/" + detail.maxHealth;
         stamina.text = detail.Stam() + "/" + detail.maxStam;
@@ -79,7 +77,47 @@ public class CharacterPanelControl : MonoBehaviour {
 
         energySlider.maxValue = detail.maxEnergy;
         energySlider.value = detail.Energy();
+    }
 
+    void LeftClick()
+    {
+        Ray ray;
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.transform.tag == "Character")
+            {
+                character = GameObject.Find(hit.transform.name);
+                Debug.Log("We hit " + hit.transform.name);
+            }
+            else
+            {
+                character = null;
+                Debug.Log("We hit nothing.");
+            }
+         }
+    }
 
+    void RightClick()
+    {
+        if (!character)
+        {
+            Debug.Log("No character selected.");
+            return;
+        }
+
+        Ray ray;
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 100f , terrainMask))
+        {
+            CharacterDetails controller = character.GetComponent<CharacterDetails>();
+
+            Debug.Log("Move to " + hit.point.x + "," +
+            hit.point.y + "," +
+            hit.point.z);
+            controller.Move(hit); 
+        }
     }
 }
